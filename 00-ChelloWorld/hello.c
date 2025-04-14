7439cf1a7d82dd284238b95cf80c532a71cf4ff8
Sofia Barbas <sbarbas@frba.utn.edu.ar>
2025-04-14 14:25:20 -0300

    Delete 00-ChelloWorld directory

--- 00-ChelloWorld/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main (){
-    FILE*f=fopen("output.txt","w+");
-    printf("Hola Sofia Barbas!");
-    fprintf(f,"Hola Sofia Barbas!");
-    fclose(f);
-    return 0;
-}
--- 00-ChelloWorld/readme.md
+++
@@ -1,2 +0,0 @@
-gcc.exe (Rev2, Built by MSYS2 project) 14.2.0
-Copyright (C) 2024 Free Software Foundation, Inc.
